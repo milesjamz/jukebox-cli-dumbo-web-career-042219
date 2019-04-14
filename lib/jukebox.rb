2dc81eaa62611da98b04cafd1765231b00ad0457
Miles Marillo <mjm03@hampshire.edu>
2019-04-14 17:56:50 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,24 +10,21 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
-def start_up
-  puts "Please enter a command:"
-  answer = gets.chomp
-end
-
 def help
-  puts "I accept the following commands:\nhelp : Displays this help message\n-list : displays a list of songs you can play\n-play : lets you choose a song to play\n- exit : exits this program"
+  puts "I accept the following commands:\n-help : Displays this help message\n-list : displays a list of songs you can play\n-play : lets you choose a song to play\n-exit : exits this program"
 end
 
 def play(songs)
-songs.each_with_index do |song, number|
 puts "Please enter a song name or number:"
-answer = gets.chomp
-if answer > 10 && answer != song
+answer = STDIN.gets.downcase.chomp
+if (1..9).to_a.include?(answer.to_i)
+  puts "Playing #{songs[answer.to_i - 1]}"
+  elsif songs.include?(answer)
+  puts "Playing #{answer}"
+  else 
   puts "Invalid input, please try again"
-end
-end
-end
+end 
+end 
 
 def list(songs)
 songs.each_with_index do |song, number|
@@ -39,9 +36,26 @@ def exit_jukebox
   puts "Goodbye"
 end
 
-def run
+def run(songs)
 help
-loop do
+answer = "" 
+while answer  
 puts "Please enter a command:"
-answer = gets.chomp
-end
+answer = STDIN.gets.strip
+case answer 
+  when 'list'
+  list(songs)
+  when 'play'
+    list(songs)
+    play(songs)
+  when 'help'
+    help 
+  when 'exit'
+    exit_jukebox
+    break 
+    else 
+      help 
+    end 
+  end 
+end 
+  run(songs)
